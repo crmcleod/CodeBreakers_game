68b3c8c8f5aa4f14f8c32fdbf3e83f0310967628
Craig McLeod <mcleod.craig@outlook.com>
2020-07-14 17:58:04 +0100

    Deploy for heroku

---
+++ client/src/components/Chat.vue
@@ -0,0 +1,36 @@
+<template>
+<div >
+    <form >
+    <!-- <input v-model="message" type="text"/> -->
+    <!-- <button v-on:click.prevent="this.send()" type="submit" value="submit">Submit</button> -->
+     </form>
+  <!-- <h1 v-for="chats in chat">{{chats}} -->
+  </h1>
+  
+  </div>
+</template>
+
+<script>
+import {eventBus} from "@/main";
+export default {
+name: 'chat',
+props: ['chat'],
+data() { return {
+    message: ""
+}},
+methods:
+{
+    send()
+    {
+        eventBus.$emit("end", this.message)
+    }
+},
+}
+
+</script>
+
+<style>
+h1{
+    color: orange;
+}
+</style>
