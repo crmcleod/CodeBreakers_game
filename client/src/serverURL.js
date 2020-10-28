1e75006b258fbb2819c98b88f85b33d7646be664
Craig McLeod <mcleod.craig@outlook.com>
2020-10-28 16:03:57 +0000

    Trying fixie

--- client/src/serverURL.js
+++ client/src/serverURL.js
@@ -1,1 +1,5 @@
-export const serverURL = "https://codebreakers-server.herokuapp.com"
+const request = require('request')
+
+const fixieRequest = request.defaults({'proxy': process.env.FIXIE_URL})
+
+export const serverURL = fixieRequest
